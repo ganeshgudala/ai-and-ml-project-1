3d7e683434612836bc460ffd24b8b05dd7fd8ba6
ganeshgudala <157883136+ganeshgudala@users.noreply.github.com>
2024-11-26 15:14:59 +0530

    Delete ml project directory

--- ml project/frontend/app/api/health/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET(request: Request, res: Response) {
-  return Response.json({ ok: Date.now().toString() });
-}
--- ml project/frontend/app/api/settings/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET(request: Request, res: Response) {
-  const data = {
-    TABLE_NAME: process.env.TABLE_NAME,
-    REGION: process.env.REGION,
-  };
-  return Response.json({ data });
-}
